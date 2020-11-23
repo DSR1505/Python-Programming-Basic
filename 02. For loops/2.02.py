1eaf2984fc3d2efb86d6aa53f015d6e34e81d4c1
DSR1505 <techdsr1505@gmail.com>
2020-11-23 11:39:44 +0530

    Chapter 2

---
+++ 02. For loops/2.02.py
@@ -0,0 +1,5 @@
+""" Write a program to fill the screen horizontally and vertically with
+your name """
+
+for i in range(500):
+	print('DSR',end='')
---
+++ 02. For loops/2.03.py
@@ -0,0 +1,5 @@
+""" Write a program that outputs 100 lines, numbered 1 to 100, each with
+your name on it. """
+
+for i in range(100):
+	print(i+1,'DSR')
